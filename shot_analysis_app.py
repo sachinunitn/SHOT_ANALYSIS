f7fa203374ebf2b4d5f65de363ee2c3a64899d02
Sachin <118995954+sachinunitn@users.noreply.github.com>
2026-04-03 18:39:57 +0200

    Change pitch type from 'statsbomb' to 'normalizedyardstogoal'

--- shot_analysis_app.py
+++ shot_analysis_app.py
@@ -262,10 +262,11 @@ def plot_pitch_with_shots(df, figsize=(10, 7)):
     fig, ax = plt.subplots(figsize=figsize)
     
     # Create pitch
-    pitch = Pitch(pitch_type='statsbomb', 
+    pitch = Pitch(pitch_type='normalizedyardstogoal', 
                   pitch_color='#22844e',
                   line_color='white',
                   linewidth=1.5
+                  half=False
     pitch.draw(ax=ax)
     
     # Separate shots by result
